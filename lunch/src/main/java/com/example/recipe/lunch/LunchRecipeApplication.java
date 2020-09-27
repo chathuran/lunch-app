f7fa65afd4316309dc856b9f373de8322df17902
dcedannoruwa@gmail.com <071@erangaDC>
2020-09-27 16:19:00 +1000

    initialize project and configure property file for database

---
+++ lunch/src/main/java/com/example/recipe/lunch/LunchRecipeApplication.java
@@ -0,0 +1,14 @@
+package com.example.recipe.lunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchRecipeApplication.class, args);
+	}
+
+}
---
+++ lunch/src/test/java/com/example/recipe/lunch/LunchRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe.lunch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
